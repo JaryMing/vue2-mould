ad6cfcbc48c5c93e1bb9b750e95b7b1bbfab40e0
wpp <906008377@qq.com>
2021-01-18 21:10:20 +0800

    vuex使用

--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -2,11 +2,11 @@
  * @Author: wpp
  * @Date: 2021-01-18 16:25:48
  * @LastEditors: wpp
- * @LastEditTime: 2021-01-18 19:14:22
+ * @LastEditTime: 2021-01-18 21:03:41
  * @FilePath: \text\src\store\mutations.ts
  */
 export default {
-  SETTESTVAL: (state: STATE) => {
-    state.test = 10;
+  SETTESTVAL: (state: STATE, val: number) => {
+    state.test = val;
   }
 };
--- src/views/About/index.vue
+++ src/views/About/index.vue
@@ -2,20 +2,31 @@
  * @Author: wpp
  * @Date: 2021-01-18 16:09:02
  * @LastEditors: wpp
- * @LastEditTime: 2021-01-18 20:30:12
+ * @LastEditTime: 2021-01-18 21:04:38
  * @FilePath: \text\src\views\About\index.vue
 -->
 <template>
   <div class="about">
-    <h1 class="text">This is an about page</h1>
+    <h1 class="text" @click="setTestValFn">
+      This is an about page {{ getTestVal }}
+    </h1>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
+import { Getter, Mutation } from "vuex-class";
 
 @Component
-export default class Layout extends Vue {}
+export default class Layout extends Vue {
+  @Getter getTestVal?: any;
+
+  @Mutation SETTESTVAL?: any;
+
+  public setTestValFn() {
+    this.SETTESTVAL(10);
+  }
+}
 </script>
 
 <style lang="less" scoped>
